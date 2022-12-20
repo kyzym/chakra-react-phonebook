bc2f8cf252b095feb8316027317b12459263f399
kyzym <102521716+kyzym@users.noreply.github.com>
2022-12-20 15:05:15 +0000

    Deploying to gh-pages from @ kyzym/goit-react-hw-08-phonebook@d0751365d99046257a4a8b7eaa3f4848a083035e 🚀

---
+++ static/js/490.f72d5cee.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[490],{3490:function(e,t,n){n.r(t),n.d(t,{default:function(){return c}});var r=n(184);function c(){return(0,r.jsxs)("div",{children:[(0,r.jsx)("title",{children:"Login"}),(0,r.jsx)("h1",{children:"Login"})]})}}}]);
+//# sourceMappingURL=490.f72d5cee.chunk.js.map
---
+++ static/js/756.220888d4.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[756],{756:function(e,t,r){r.r(t),r.d(t,{default:function(){return u}});var n=r(184);function u(){return(0,n.jsx)("h1",{children:"Register"})}}}]);
+//# sourceMappingURL=756.220888d4.chunk.js.map
