f763c184f25729d354dbda7bd5886d2934b75699
Denis Kyzym <102521716+kyzym@users.noreply.github.com>
2022-12-26 12:25:38 +0100

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,5 @@
-import { Box } from '@chakra-ui/layout';
-import { useColorModeValue } from '@chakra-ui/react';
-import CallToActionWithIllustration from 'components/Hero';
+import Hero from 'components/Hero';
 
 export default function Home() {
-  const bgColor = useColorModeValue('gray.200', 'gray.700');
-  return (
-    <Box
-      as="section"
-      className="home"
-      bg={bgColor}
-      minH={{ base: '78vh', md: '83.7vh', lg: '85vh' }}
-    >
-      <CallToActionWithIllustration />
-    </Box>
-  );
+  return <Hero />;
 }
